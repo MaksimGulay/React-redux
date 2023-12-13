81cace222922c826b681f448a2dba81638d7827b
Maksim Gulay <maksimgulaj@gmail.com>
2023-12-13 02:23:17 +0200

    learn state and props

--- src/components/QuizCard/QuizCard.jsx
+++ src/components/QuizCard/QuizCard.jsx
@@ -1,13 +1,19 @@
 import { Topic, Wrapper, Text } from './QuizCard.styled';
 
 // глибока деструктутизація
-export const QuizCard = ({ item: { topic, level, time, questions } }) => {
+export const QuizCard = ({
+  item: { id, topic, level, time, questions },
+  onDelete,
+}) => {
   return (
     <Wrapper>
       <Topic>{topic}</Topic>
       <Text>Level: {level}</Text>
       <Text>Time: {time} min</Text>
       <Text>Questions: {questions}</Text>
+      <div>
+        <button onClick={() => onDelete(id)}>Delete</button>
+      </div>
     </Wrapper>
   );
 };
